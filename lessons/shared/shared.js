9d058c76e6fda2b757ef498459b6d02b5f676229
Daniel Ramos <daniel.ramosg@gmail.com>
2019-10-03 11:29:13 +0200

    Teaching notes ON by default

--- lessons/shared/shared.js
+++ lessons/shared/shared.js
@@ -5,6 +5,8 @@
 
 import {Browser, $body, $} from '@mathigon/boost';
 
+// By default the Teaching notes are OFF
+/*
 let showTeachingNotes = !!(+Browser.getCookie('show_teaching_notes'));
 if (showTeachingNotes) $body.addClass('show-teaching-notes');
 
@@ -12,4 +14,18 @@ $('.toggle-teacher-notes').on('click', () => {
   showTeachingNotes = !showTeachingNotes;
   Browser.setCookie('show_teaching_notes', showTeachingNotes ? '1' : '0');
   $body.setClass('show-teaching-notes', showTeachingNotes);
+});*/
+
+
+// By default the Teaching notes are ON
+
+let hideTeachingNotes = !!(+Browser.getCookie('hide_teaching_notes'));
+
+$body.addClass('show-teaching-notes');
+if (hideTeachingNotes)  $body.setClass('show-teaching-notes', !hideTeachingNotes);
+
+$('.toggle-teacher-notes').on('click', () => {
+  hideTeachingNotes = !hideTeachingNotes;
+  Browser.setCookie('hide_teaching_notes', hideTeachingNotes ? '1' : '0');
+  $body.setClass('show-teaching-notes', !hideTeachingNotes);
 });
